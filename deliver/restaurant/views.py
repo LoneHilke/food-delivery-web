c80f6e1a6912175748969ab189690b6fc69511c5
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-05-11 21:26:23 +0200

    part 6
    part 6
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- deliver/restaurant/views.py
+++ deliver/restaurant/views.py
@@ -11,18 +11,45 @@ class Dashboard(LoginRequiredMixin,UserPassesTestMixin, View):
         orders = OrderModel.objects.filter(created_on__year=today.year, created_on__month=today.month, created_on__day=today.day)
 
         #loop through orders
+        unshipped_orders = []
         total_revenue = 0
         for order in orders:
             total_revenue += order.price
 
+            if not order.is_shipped:
+                unshipped_orders.append(order)
+
         #pas total number of orders
         context = {
-            'orders': orders,
+            'orders': unshipped_orders,
             'total_revenue': total_revenue,
             'total_orders': len(orders),
             }
 
         return render(request, 'restaurant/dashboard.html', context)
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='LoJe').exists()
+
+class OrderDetails(LoginRequiredMixin, UserPassesTestMixin, View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+        context = {
+            'order': order,
+        }
+
+        return render(request, 'restaurant/order-details.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+        order.is_shipped = True
+        order.save()
+
+        context = {
+            'order': order,
+        }
+
+        return render(request, 'restaurant/order-details.html', context)
 
     def test_func(self):
         return self.request.user.groups.filter(name='LoJe').exists()
